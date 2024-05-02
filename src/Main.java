fad469a7d04ac3f2f110c5b1431c7d50314f763d
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2024-05-02 10:04:15 +0200

    java condingame interview - update code

--- src/Main.java
+++ src/Main.java
@@ -15,12 +15,29 @@ public class Main {
                 result.add(word);
             }
         }
-        System.out.println(result);
+        System.out.println("result= " + result);
 
         /*String[] resultArray = Arrays.stream(array)
                 .filter(word -> word.contains("ea"))
                 .toArray(String[]::new);*/
 
+        // 2- In a given array [all, in, the, beat, box]
+        // return an array that matches one letter "ea"
+
+        String[] words2 = {"all", "in", "the", "beat", "box"};
+        String letters2 = "ea";
+        List<Object> result2 = new ArrayList<>();
+
+        for (String word : words2) {
+            for (int i = 0; i < letters2.toCharArray().length; i++) {
+                for (char character : word.toCharArray()) {
+                    if (letters2.toCharArray()[i] == character){
+                        if(!result2.contains(word)) result2.add(word);
+                    }
+                }
+            }
+        }
+        System.out.println("result2= "+result2);
 
     }
 }
